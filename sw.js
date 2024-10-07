9ca73574ff5548bca51090319bdb021ab5c47ee0
unknown <1500619430@qq.com>
2024-10-07 11:22:56 +0800

    Site updated: 2024-10-07 11:22:53

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","e38b9ef5efe4a1f446c45d99f72eec02"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
